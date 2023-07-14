24c0e25a40562480e60233ef5d0e407e5136fd12
tlarbals824 <simkm0824@naver.com>
2023-07-14 10:41:11 +0900

    [2주차] 2. 옵저버 패턴 - 심규민

--- ch02/심규민/code/observerpattern/subject/Subject.java
+++ ch02/심규민/code/observerpattern/subject/Subject.java
@@ -4,7 +4,8 @@ package com.sim.observerpattern.subject;
 import com.sim.observerpattern.observer.Observer;
 
 public interface Subject {
-    public void registerObserver(Observer o);
-    public void removeObserver(Observer o);
+    public void registerObserver(Observer<? extends Status> o);
+
+    public void removeObserver(Observer<? extends Status> o);
     public void notifyObservers();
 }
